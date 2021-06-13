c158c4473a2a576b24e383114e610fcd0536dd11
Calvin Gerling <calvin@gerling.sh>
2021-06-13 11:35:59 -0300

    add pagination to player controller

--- lib/nfl_rushing_web/controllers/player_controller.ex
+++ lib/nfl_rushing_web/controllers/player_controller.ex
@@ -4,11 +4,37 @@ defmodule NflRushingWeb.PlayerController do
   alias NflRushing.League
 
   @spec index(Plug.Conn.t(), map) :: Plug.Conn.t()
-  def index(conn, _) do
-    players = League.list_players()
+  def index(conn, params) do
+    players =
+      params
+      |> get_pagination_params()
+      |> League.list_players()
 
     conn
     |> put_status(:ok)
     |> render("index.json", players: players)
+  end
+
+  defp get_pagination_params(%{"page" => page, "page_size" => page_size}) do
+    %{
+      page: String.to_integer(page),
+      page_size: String.to_integer(page_size)
+    }
+  end
+
+  defp get_pagination_params(%{"page" => page}) do
+    %{
+      page: String.to_integer(page)
+    }
+  end
+
+  defp get_pagination_params(%{"page_size" => page_size}) do
+    %{
+      page_size: String.to_integer(page_size)
+    }
+  end
+
+  defp get_pagination_params(%{}) do
+    %{}
   end
 end
--- test/nfl_rushing_web/controllers/player_controller_test.exs
+++ test/nfl_rushing_web/controllers/player_controller_test.exs
@@ -2,7 +2,7 @@ defmodule NflRushingWeb.PlayerControllerTest do
   use NflRushingWeb.ConnCase, async: true
 
   describe "GET /players" do
-    test "returns a list with all players", %{conn: conn} do
+    test "returns a list of players with statistic and team information", %{conn: conn} do
       insert_list(5, :player)
 
       assert response =
@@ -11,12 +11,47 @@ defmodule NflRushingWeb.PlayerControllerTest do
                |> json_response(:ok)
 
       assert %{"data" => players} = response
-      assert Enum.count(players) == 5
 
       assert Enum.all?(
                players,
                &match?(%{"name" => _, "position" => _, "statistic" => _, "team" => _}, &1)
              )
+    end
+
+    test "returns a page of players with default size", %{conn: conn} do
+      insert_list(50, :player)
+
+      assert response =
+               conn
+               |> get(Routes.player_path(conn, :index))
+               |> json_response(:ok)
+
+      %{"data" => players} = response
+      assert Enum.count(players) == 30
+    end
+
+    test "returns a page of players with a custom size", %{conn: conn} do
+      insert_list(50, :player)
+
+      params = %{"page_size" => 50}
+
+      assert response =
+               conn
+               |> get(Routes.player_path(conn, :index, params))
+               |> json_response(:ok)
+
+      %{"data" => players} = response
+      assert Enum.count(players) == 50
+    end
+
+    test "returns an empty list of players when no player was found", %{conn: conn} do
+      assert response =
+               conn
+               |> get(Routes.player_path(conn, :index))
+               |> json_response(:ok)
+
+      %{"data" => players} = response
+      assert Enum.empty?(players)
     end
   end
 end
