a159db7432a503f5d6a04dbba225002d35c41853
Calvin Gerling <calvin@gerling.sh>
2021-06-13 11:16:44 -0300

    add pagination when listing players

--- lib/nfl_rushing/league.ex
+++ lib/nfl_rushing/league.ex
@@ -1,10 +1,19 @@
 defmodule NflRushing.League do
   alias NflRushing.League.Player
+  alias NflRushing.Query
   alias NflRushing.Repo
 
-  @spec list_players() :: list(Player.t())
-  def list_players do
+  @default_page 1
+  @default_page_size 30
+
+  @spec list_players(map) :: list(Player.t())
+  def list_players(params \\ %{}) do
+    page = Map.get(params, :page, @default_page)
+    page_size = Map.get(params, :page_size, @default_page_size)
+
     Player
+    |> Query.sort_by(:inserted_at, :desc)
+    |> Query.paginate(page: page, page_size: page_size)
     |> Repo.all()
     |> Repo.preload([:statistic, :team])
   end
--- test/nfl_rushing/league_test.exs
+++ test/nfl_rushing/league_test.exs
@@ -3,23 +3,57 @@ defmodule NflRushing.LeagueTest do
 
   alias NflRushing.League
 
-  describe "list_players/0" do
-    test "returns a list of all players" do
-      players = insert_list(5, :player)
+  describe "list_players/1" do
+    test "returns a subset of all existing players" do
+      players = insert_list(50, :player)
 
       fetched_players = League.list_players()
 
-      assert Enum.count(fetched_players) == 5
+      assert Enum.count(fetched_players) == 30
 
       assert Enum.all?(fetched_players, fn fetched_player ->
                Enum.find(players, &(&1.id == fetched_player.id))
              end)
+    end
+
+    test "returns a subset of all players sorted by the newest to the oldest" do
+      insert_list(50, :player)
+
+      fetched_players = League.list_players()
+
+      sorted_fetched_players = Enum.sort_by(fetched_players, & &1.inserted_at, :desc)
+
+      assert sorted_fetched_players == fetched_players
+    end
+
+    test "returns a specific subset of players" do
+      insert_list(50, :player)
+
+      params = %{page: 2}
+      fetched_players = League.list_players(params)
+
+      assert Enum.count(fetched_players) == 20
+    end
+
+    test "returns a subset of players with custom size" do
+      insert_list(50, :player)
+
+      params = %{page: 1, page_size: 50}
+      fetched_players = League.list_players(params)
+
+      assert Enum.count(fetched_players) == 50
+    end
+
+    test "returns a list of players with statistic and team information" do
+      insert_list(50, :player)
+
+      fetched_players = League.list_players()
 
       assert Enum.all?(fetched_players, &Ecto.assoc_loaded?(&1.team))
       assert Enum.all?(fetched_players, &Ecto.assoc_loaded?(&1.statistic))
     end
 
-    test "returns an empty list when there is no players" do
+    test "returns an empty list when no player exists" do
       fetched_players = League.list_players()
 
       assert Enum.empty?(fetched_players)
