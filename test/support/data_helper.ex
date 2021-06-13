25ad0caa51cca2994e87d9ab8b4aefce2dfcb3da
Calvin Gerling <calvin@gerling.sh>
2021-06-13 12:44:05 -0300

    move data case functions to a new module

---
+++ test/support/data_helper.ex
@@ -0,0 +1,20 @@
+defmodule NflRushing.DataHelper do
+  alias Ecto.Changeset
+
+  @doc """
+  A helper that transforms changeset errors into a map of messages.
+
+      assert {:error, changeset} = Accounts.create_user(%{password: "short"})
+      assert "password is too short" in errors_on(changeset).password
+      assert %{password: ["password is too short"]} = errors_on(changeset)
+
+  """
+  @spec errors_on(Changeset.t()) :: %{required(atom) => [String.t() | map]}
+  def errors_on(changeset) do
+    Changeset.traverse_errors(changeset, fn {message, opts} ->
+      Regex.replace(~r"%{(\w+)}", message, fn _, key ->
+        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+      end)
+    end)
+  end
+end
