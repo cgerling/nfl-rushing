8d92e0cf66259a539924648ffb2d286ce441a1d4
Calvin Gerling <calvin@gerling.sh>
2021-06-13 16:26:19 -0300

    remove unnecessary Map.get/3 from list players

--- lib/nfl_rushing/league.ex
+++ lib/nfl_rushing/league.ex
@@ -3,17 +3,11 @@ defmodule NflRushing.League do
   alias NflRushing.Query
   alias NflRushing.Repo
 
-  @default_page 1
-  @default_page_size 30
-
   @spec list_players(map) :: list(Player.t())
   def list_players(params \\ %{}) do
-    page = Map.get(params, :page, @default_page)
-    page_size = Map.get(params, :page_size, @default_page_size)
-
     Player
     |> Query.sort_by(:inserted_at, :desc)
-    |> Query.paginate(page: page, page_size: page_size)
+    |> Query.paginate(page: params.page, page_size: params.page_size)
     |> Repo.all()
     |> Repo.preload([:statistic, :team])
   end
