4eaef3f5c5eef5df852d1c1854454ffb7fe8f967
Calvin Gerling <calvin@gerling.sh>
2021-06-13 17:05:57 -0300

    use search object um player controller

--- lib/nfl_rushing_web/controllers/player_controller.ex
+++ lib/nfl_rushing_web/controllers/player_controller.ex
@@ -3,17 +3,29 @@ defmodule NflRushingWeb.PlayerController do
 
   alias NflRushing.League
   alias NflRushingWeb.PageObject
+  alias NflRushingWeb.SearchObject
 
   action_fallback(NflRushingWeb.FallbackController)
 
   @spec index(Plug.Conn.t(), map) :: Plug.Conn.t()
   def index(conn, params) do
-    with {:ok, page_object} <- PageObject.from_params(params) do
-      players = League.list_players(page_object)
+    with {:ok, page_object} <- PageObject.from_params(params),
+         {:ok, search_object} <- SearchObject.from_params(params) do
+      players =
+        page_object
+        |> merge_objects(search_object)
+        |> League.list_players()
 
       conn
       |> put_status(:ok)
       |> render("index.json", players: players)
     end
+  end
+
+  defp merge_objects(object_a, object_b) do
+    object_a_map = Map.from_struct(object_a)
+    object_b_map = Map.from_struct(object_b)
+
+    Map.merge(object_a_map, object_b_map)
   end
 end
