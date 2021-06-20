2ff6bd131c8ef4681669f8b42dfe789911231b47
Calvin Gerling <calvin@gerling.sh>
2021-06-20 18:56:21 -0300

    create function to export league players as csv

--- lib/nfl_rushing/league.ex
+++ lib/nfl_rushing/league.ex
@@ -1,4 +1,5 @@
 defmodule NflRushing.League do
+  alias NflRushing.Exporter
   alias NflRushing.League.Player
   alias NflRushing.Query
   alias NflRushing.Repo
@@ -13,5 +14,12 @@ defmodule NflRushing.League do
     |> Query.paginate(page: page.page, page_size: page.page_size)
     |> Repo.all()
     |> Repo.preload([:statistic, :team])
+  end
+
+  @spec export_players(map) :: String.t()
+  def export_players(%{} = params) do
+    params
+    |> list_players()
+    |> Exporter.export_players()
   end
 end
