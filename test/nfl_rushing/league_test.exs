211c7d47f6edd6124129b95cf2df2ad366ea734a
Calvin Gerling <calvin@gerling.sh>
2021-07-01 23:13:11 -0300

    move build_csv/1 function to DataHelper module

--- test/nfl_rushing/league_test.exs
+++ test/nfl_rushing/league_test.exs
@@ -201,48 +201,6 @@ defmodule NflRushing.LeagueTest do
     end
   end
 
-  defp build_csv(players) when is_list(players) do
-    headers =
-      "name,position,abbreviation,average_rushing_attempts_per_game,average_rushing_yards_per_attempt,longest_rush,rushing_attempts,rushing_first_downs,rushing_first_downs_percentage,rushing_fourty_yards_each,rushing_fumbles,rushing_twenty_yards_each,rushing_yards_per_game,total_rushing_touchdowns,total_rushing_yards"
-
-    entries = convert_to_csv_entries(players)
-
-    "#{headers}\r\n#{entries}"
-  end
-
-  defp convert_to_csv_entries([]), do: ""
-
-  defp convert_to_csv_entries(players) do
-    entries =
-      players
-      |> Enum.map(fn player ->
-        %{team: team, statistic: statistic} = player
-
-        entry = [
-          player.name,
-          player.position,
-          team.abbreviation,
-          statistic.average_rushing_attempts_per_game,
-          statistic.average_rushing_yards_per_attempt,
-          statistic.longest_rush,
-          statistic.rushing_attempts,
-          statistic.rushing_first_downs,
-          statistic.rushing_first_downs_percentage,
-          statistic.rushing_fourty_yards_each,
-          statistic.rushing_fumbles,
-          statistic.rushing_twenty_yards_each,
-          statistic.rushing_yards_per_game,
-          statistic.total_rushing_touchdowns,
-          statistic.total_rushing_yards
-        ]
-
-        Enum.join(entry, ",")
-      end)
-      |> Enum.join("\r\n")
-
-    "#{entries}\r\n"
-  end
-
   describe "import_team/1" do
     test "returns a team when params are valid" do
       params = params_for(:team)
--- test/support/data_helper.ex
+++ test/support/data_helper.ex
@@ -17,4 +17,47 @@ defmodule NflRushing.DataHelper do
       end)
     end)
   end
+
+  @spec build_csv(list(Player.t())) :: String.t()
+  def build_csv(players) when is_list(players) do
+    headers =
+      "name,position,abbreviation,average_rushing_attempts_per_game,average_rushing_yards_per_attempt,longest_rush,rushing_attempts,rushing_first_downs,rushing_first_downs_percentage,rushing_fourty_yards_each,rushing_fumbles,rushing_twenty_yards_each,rushing_yards_per_game,total_rushing_touchdowns,total_rushing_yards"
+
+    entries = convert_to_csv_entries(players)
+
+    "#{headers}\r\n#{entries}"
+  end
+
+  defp convert_to_csv_entries([]), do: ""
+
+  defp convert_to_csv_entries(players) do
+    entries =
+      players
+      |> Enum.map(fn player ->
+        %{team: team, statistic: statistic} = player
+
+        entry = [
+          player.name,
+          player.position,
+          team.abbreviation,
+          statistic.average_rushing_attempts_per_game,
+          statistic.average_rushing_yards_per_attempt,
+          statistic.longest_rush,
+          statistic.rushing_attempts,
+          statistic.rushing_first_downs,
+          statistic.rushing_first_downs_percentage,
+          statistic.rushing_fourty_yards_each,
+          statistic.rushing_fumbles,
+          statistic.rushing_twenty_yards_each,
+          statistic.rushing_yards_per_game,
+          statistic.total_rushing_touchdowns,
+          statistic.total_rushing_yards
+        ]
+
+        Enum.join(entry, ",")
+      end)
+      |> Enum.join("\r\n")
+
+    "#{entries}\r\n"
+  end
 end
