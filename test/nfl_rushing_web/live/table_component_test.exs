0aa05358521bcdcb7192d7a278816cdbec3ecc48
Calvin Gerling <calvin@gerling.sh>
2021-07-02 09:15:01 -0300

    use floki for table component tests

--- test/nfl_rushing_web/live/table_component_test.exs
+++ test/nfl_rushing_web/live/table_component_test.exs
@@ -9,28 +9,23 @@ defmodule NflRushingWeb.TableComponentTest do
     test "renders a table tag with assigns" do
       headers = ["Number"]
       entries = Enum.map(1..4, &List.wrap/1)
-      table_component = render_component(TableComponent, headers: headers, entries: entries)
 
-      assert table_component =~ ~s{<table class=\"min-w-full\">}
-      assert table_component =~ ~s{<thead class=\"border-b-8 border-opacity-0\">}
+      assert {:ok, table_component} =
+               TableComponent
+               |> render_component(headers: headers, entries: entries)
+               |> Floki.parse_fragment()
 
-      assert table_component =~
-               ~s{<tbody class="border-opacity-0 border-t-8 divide-y divide-gray-200">}
+      assert [table_element] = Floki.find(table_component, "table")
 
-      assert table_component =~
-               ~s{<th scope="col" class="bg-gray-100 font-semibold px-3 py-5 rounded-l-md text-gray-500 text-center text-sm tracking-wide">Number</th>}
+      assert ["Number"] ==
+               table_element
+               |> Floki.find("thead tr th")
+               |> Enum.map(&Floki.text/1)
 
-      assert table_component =~
-               ~s{<td class="font-normal px-3 py-4 text-gray-600 text-center text-sm whitespace-nowrap">1</td>}
-
-      assert table_component =~
-               ~s{<td class="font-normal px-3 py-4 text-gray-600 text-center text-sm whitespace-nowrap">2</td>}
-
-      assert table_component =~
-               ~s{<td class="font-normal px-3 py-4 text-gray-600 text-center text-sm whitespace-nowrap">3</td>}
-
-      assert table_component =~
-               ~s{<td class="font-normal px-3 py-4 text-gray-600 text-center text-sm whitespace-nowrap">4</td>}
+      assert ["1", "2", "3", "4"] ==
+               table_element
+               |> Floki.find("tbody tr td")
+               |> Enum.map(&Floki.text/1)
     end
 
     test "renders a table tag with custom mapper" do
@@ -38,38 +33,36 @@ defmodule NflRushingWeb.TableComponentTest do
       entries = Enum.map(1..4, &List.wrap/1)
       mapper = fn entry -> Enum.map(entry, &Kernel.*(&1, 2)) end
 
-      table_component =
-        render_component(TableComponent, headers: headers, entries: entries, mapper: mapper)
+      assert {:ok, table_component} =
+               TableComponent
+               |> render_component(headers: headers, entries: entries, mapper: mapper)
+               |> Floki.parse_fragment()
 
-      assert table_component =~
-               ~s{<td class="font-normal px-3 py-4 text-gray-600 text-center text-sm whitespace-nowrap">2</td>}
+      assert [table_element] = Floki.find(table_component, "table")
 
-      assert table_component =~
-               ~s{<td class="font-normal px-3 py-4 text-gray-600 text-center text-sm whitespace-nowrap">4</td>}
-
-      assert table_component =~
-               ~s{<td class="font-normal px-3 py-4 text-gray-600 text-center text-sm whitespace-nowrap">6</td>}
-
-      assert table_component =~
-               ~s{<td class="font-normal px-3 py-4 text-gray-600 text-center text-sm whitespace-nowrap">8</td>}
+      assert ["2", "4", "6", "8"] ==
+               table_element
+               |> Floki.find("tbody tr td")
+               |> Enum.map(&Floki.text/1)
     end
 
-    test "renders a table tag without content when entries are not informed" do
+    test "renders a table tag with no content body when entries are empty" do
       headers = ["Number"]
       entries = []
-      table_component = render_component(TableComponent, headers: headers, entries: entries)
 
-      assert table_component =~ ~s{<table class=\"min-w-full\">}
-      assert table_component =~ ~s{<thead class=\"border-b-8 border-opacity-0\">}
+      assert {:ok, table_component} =
+               TableComponent
+               |> render_component(headers: headers, entries: entries)
+               |> Floki.parse_fragment()
 
-      assert table_component =~
-               ~s{<tbody class="border-opacity-0 border-t-8 divide-y divide-gray-200">}
+      assert [table_element] = Floki.find(table_component, "table")
 
-      assert table_component =~
-               ~s{<th scope="col" class="bg-gray-100 font-semibold px-3 py-5 rounded-l-md text-gray-500 text-center text-sm tracking-wide">Number</th>}
+      assert ["Number"] ==
+               table_element
+               |> Floki.find("thead tr th")
+               |> Enum.map(&Floki.text/1)
 
-      refute table_component =~
-               ~s{<td class="font-normal px-3 py-4 text-gray-600 text-center text-sm whitespace-nowrap">}
+      assert [] == Floki.find(table_element, "tbody tr td")
     end
   end
 end
