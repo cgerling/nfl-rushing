f3899c0f3c2a5d057141633eabdddf6405d7a02b
Calvin Gerling <calvin@gerling.sh>
2021-07-04 19:12:42 -0300

    only change config when running in production

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,36 +1,38 @@
 import Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+if config_env() == :prod do
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
 
-config :nfl_rushing, NflRushing.Repo,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  config :nfl_rushing, NflRushing.Repo,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
 
-hostname =
-  System.get_env("HOSTNAME") ||
-    raise """
-    environment variable HOSTNAME is missing.
-    For example: example.com
-    """
+  hostname =
+    System.get_env("HOSTNAME") ||
+      raise """
+      environment variable HOSTNAME is missing.
+      For example: example.com
+      """
 
-port = String.to_integer(System.get_env("PORT") || "4000")
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
-config :nfl_rushing, NflRushingWeb.Endpoint,
-  http: [
-    port: port,
-    transport_options: [socket_opts: [:inet6]]
-  ],
-  secret_key_base: secret_key_base,
-  url: [host: hostname, port: port]
+  config :nfl_rushing, NflRushingWeb.Endpoint,
+    http: [
+      port: port,
+      transport_options: [socket_opts: [:inet6]]
+    ],
+    secret_key_base: secret_key_base,
+    url: [host: hostname, port: port]
+end
