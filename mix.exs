86de52cd0df205c6251fafef16ad2db09c0b5836
Calvin Gerling <calvin@gerling.sh>
2021-07-05 10:33:52 -0300

    bump elixir version to 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NflRushing.MixProject do
     [
       app: :nfl_rushing,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
