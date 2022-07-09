a4e6e4281c79035c24a802d99a92b833ca5391a1
Calvin Gerling <calvin@gerling.sh>
2022-07-09 20:18:17 -0300

    wip

--- lib/nfl_rushing/league/player_statistic.ex
+++ lib/nfl_rushing/league/player_statistic.ex
@@ -1,14 +1,13 @@
 defmodule NflRushing.League.PlayerStatistic do
   use NflRushing.Schema
 
-  alias NflRushing.League.Player
-
   @type t :: %__MODULE__{
           average_rushing_attempts_per_game: float,
           average_rushing_yards_per_attempt: float,
           inserted_at: NaiveDateTime.t() | nil,
           longest_rush: String.t(),
-          player: Player.t(),
+          player_name: String.t(),
+          player_position: String.t(),
           rushing_attempts: integer,
           rushing_first_downs: integer,
           rushing_first_downs_percentage: float,
@@ -25,6 +24,8 @@ defmodule NflRushing.League.PlayerStatistic do
     :average_rushing_attempts_per_game,
     :average_rushing_yards_per_attempt,
     :longest_rush,
+    :player_name,
+    :player_position,
     :rushing_attempts,
     :rushing_first_downs,
     :rushing_first_downs_percentage,
@@ -41,6 +42,8 @@ defmodule NflRushing.League.PlayerStatistic do
     field :average_rushing_attempts_per_game, :float
     field :average_rushing_yards_per_attempt, :float
     field :longest_rush, :string
+    field :player_name, :string
+    field :player_position, :string
     field :rushing_attempts, :integer
     field :rushing_first_downs, :integer
     field :rushing_first_downs_percentage, :float
@@ -52,8 +55,6 @@ defmodule NflRushing.League.PlayerStatistic do
     field :total_rushing_yards, :integer
 
     timestamps()
-
-    belongs_to :player, Player
   end
 
   @spec changeset(%__MODULE__{}, map) :: Changeset.t()
@@ -61,6 +62,5 @@ defmodule NflRushing.League.PlayerStatistic do
     struct
     |> cast(params, @fields)
     |> validate_required(@required_fields)
-    |> assoc_constraint(:player)
   end
 end
